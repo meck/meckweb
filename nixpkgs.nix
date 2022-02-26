ac0613a6b06c4de526d119b9a128d62254366a8e
meck <Johan@meck.se>
2022-02-26 15:49:03 +0100

    redeploy

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,9 +1,11 @@
-import (builtins.fetchTarball {
-  name = "nixos-unstable-2021-02-04";
-  url =
-    "https://github.com/nixos/nixpkgs/archive/7cbec40a09533dd9c525d6ab94dddfe77768101a.tar.gz";
-  sha256 = "006fns0kxs9n32cg6f4p0zyaxxsyidwsa152flpsbaky1c6drn96";
-}) {
+import
+  (builtins.fetchTarball {
+    name = "nixos-unstable-2021-02-04";
+    url =
+      "https://github.com/nixos/nixpkgs/archive/7cbec40a09533dd9c525d6ab94dddfe77768101a.tar.gz";
+    sha256 = "006fns0kxs9n32cg6f4p0zyaxxsyidwsa152flpsbaky1c6drn96";
+  })
+{
   overlays = [
 
     (self: super: {
